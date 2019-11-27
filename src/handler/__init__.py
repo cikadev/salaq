244624c951d9bf1cd80e4d9eb0388ecc0803a4d5
Andra Antariksa Prihadi <andra.antariksa@gmail.com>
2019-11-27 15:09:54 +0700

    Done modularize the project

---
+++ src/handler/__init__.py
@@ -0,0 +1,81 @@
+from flask import render_template, request, flash, url_for, redirect
+import sqlalchemy
+import json
+import flask_login
+from src import login_manager, app
+from src.models.users import Users
+
+@login_manager.user_loader
+def load_user(email):
+    if email is not None:
+        return models.Users.where_email(email)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('You must be logged in to view that page.')
+    return redirect(url_for("signin"))
+
+
+@app.route('/')
+def home():
+    return render_template("home.html", title="Home")
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect(url_for("signin"))
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html", title="Signin")
+
+@app.route("/api/signin", methods=["POST"])
+def signin_API():
+    email = request.form['email']
+    password = request.form['password']
+
+    signin_user = Users.where_email_password(email, password)
+
+    success = False
+    if signin_user is not None:
+        flask_login.login_user(signin_user)
+        success = True
+
+    return json.dumps({
+        "success": success,
+    })
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html", title="Signup")
+
+@app.route("/me")
+@flask_login.login_required
+def me():
+    current_user = flask_login.current_user
+    return f"Hello, {current_user.email}"
+
+@app.route('/api/signup', methods=["POST"])
+def signup_API():
+    email = request.form['email']
+    password = request.form['password']
+
+    user_temp = Users()
+    user_temp.email = email
+    user_temp.password = password
+    success = True
+
+    try:
+        user_temp.create()
+    except sqlalchemy.exc.IntegrityError:
+        success = False
+    finally:
+        return json.dumps({
+            "success": success,
+        })
+
+@app.route('/@<username>/<product_slug>')
+def product(username, product_slug):
+    return render_template("product.html", title="Product")
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,10 +7,3 @@ engine = create_engine("postgres://postgres:123@localhost:5432/bojonegoro", echo
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
-
-
-class Users(Base):
-    __tablename__ = 'users'
-    email = Column(String, primary_key=True)
-    password = Column(String)
-    name = Column(String)
