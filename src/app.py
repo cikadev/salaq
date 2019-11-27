03ce5f4156b5378b04c98eaa86c05ddacfd3b201
Andra Antariksa Prihadi <andra.antariksa@gmail.com>
2019-11-27 14:43:57 +0700

    Done authentication

--- src/app.py
+++ src/app.py
@@ -1,37 +1,114 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, request, flash, url_for, redirect
 import flask_login
-
-import models
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+import json
 
 # Initializing Flask
 app = Flask(__name__)
+app.secret_key = "Cikadev"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://postgres:123@localhost:5432/bojonegoro"
+db = SQLAlchemy(app)
+
+
+class Users(flask_login.UserMixin, db.Model):
+    __tablename__ = 'users'
+    email = db.Column(db.String, primary_key=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def get_id(self):
+        return self.email
+
+    @staticmethod
+    def where_email(email):
+        return Users.query.filter_by(email=email).first()
+
+    @staticmethod
+    def where_email_password(email, password):
+        return Users.query.filter_by(email=email, password=password).first()
+
 
 # Authentication
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
-#class User(flask_login.UserMixin):
-#    pass
+
+@login_manager.user_loader
+def load_user(email):
+    if email is not None:
+        return Users.where_email(email)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    flash('You must be logged in to view that page.')
+    return redirect(url_for("signin"))
+
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="Home")
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return redirect(url_for("signin"))
 
 @app.route('/signin')
 def signin():
-    return render_template("signin.html")
+    return render_template("signin.html", title="Signin")
+
+@app.route("/api/signin", methods=["POST"])
+def signin_API():
+    email = request.form['email']
+    password = request.form['password']
+
+    signin_user = Users.where_email_password(email, password)
+
+    success = False
+    if signin_user is not None:
+        flask_login.login_user(signin_user)
+        success = True
+
+    return json.dumps({
+        "success": success,
+    })
 
 @app.route('/signup')
 def signup():
-    return render_template("signup.html")
+    return render_template("signup.html", title="Signup")
 
-@app.route('/api/signup')
+@app.route("/me")
+@flask_login.login_required
+def me():
+    current_user = flask_login.current_user
+    return f"Hello, {current_user.email}"
+
+@app.route('/api/signup', methods=["POST"])
 def signup_API():
-    # user_temp = models.User(email="andra.antariksa@gmail.com", password="andra123")
-    # models.session.add(user_temp)
-    # models.session.commit()
-    return "OK"
+    email = request.form['email']
+    password = request.form['password']
+
+    user_temp = Users()
+    user_temp.email = email
+    user_temp.password = password
+    success = True
+
+    try:
+        user_temp.create()
+    except sqlalchemy.exc.IntegrityError:
+        success = False
+    finally:
+        return json.dumps({
+            "success": success,
+        })
 
 @app.route('/@<username>/<product_slug>')
 def product(username, product_slug):
-    return render_template("product.html")
+    return render_template("product.html", title="Product")
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,17 +2,15 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgres://postgres:123@localhost:5432/", echo=True)
+engine = create_engine("postgres://postgres:123@localhost:5432/bojonegoro", echo=True)
 
 Base = declarative_base()
-Session = sessionmaker(Base)
+Session = sessionmaker(bind=engine)
 session = Session()
 
-class User(Base):
+
+class Users(Base):
     __tablename__ = 'users'
     email = Column(String, primary_key=True)
     password = Column(String)
     name = Column(String)
-
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (self.name, self.fullname, self.nickname)
